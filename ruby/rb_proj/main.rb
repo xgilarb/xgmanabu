72e9c62fb7016fa6e5188f7b929145c0122fd7d7
xpynim <xpynim@gmail.com>
2024-06-12 08:25:05 +0700

    pyn1445dzh05:updatemain.rb

--- ruby/rb_proj/main.rb
+++ ruby/rb_proj/main.rb
@@ -8,3 +8,34 @@ class Colorizer
 end
 
 Colorizer.print_colored_text
+
+# tiga jenis iterasi umum
+
+# times-loop
+3.times do
+	puts "ayo kita berhitung"
+end
+
+i = 0
+# while-loop
+while i <= 10 do
+	puts "hitungan ke #{i}"
+	i += 1
+end
+
+# for-loop
+for j in 11..20  do
+	puts "hitungan ke #{j}"
+end
+
+# each_method-loop
+(21..30).each do |k|
+	puts "hitungan ke #{k}"
+end
+
+# until-loop
+l = 31
+until l > 40 do
+	puts "hitungan ke #{l}"
+	l += 1
+end
