5b86bc5bd3267f25a29553bdc4c8ebedcaec56ac
xpynim <xpynim@gmail.com>
2024-07-17 16:22:02 +0700

    update main.rb

--- ruby/rb_proj/main.rb
+++ ruby/rb_proj/main.rb
@@ -1,4 +1,4 @@
-require 'colorize'
+require "colorize"
 
 puts "this is ruby learning"
 puts "this is ruby learning"
@@ -14,10 +14,10 @@ puts "this is ruby learning"
 puts "this is ruby learning"
 
 class Colorizer
-	def self.print_colored_text
-		puts "this is blue text".colorize(:blue)
-		puts "this is red text".colorize(:red)
-	end
+  def self.print_colored_text
+    puts "this is blue text".colorize(:blue)
+    puts "this is red text".colorize(:red)
+  end
 end
 
 Colorizer.print_colored_text
@@ -25,30 +25,30 @@ Colorizer.print_colored_text
 # tiga jenis iterasi umum
 
 # times-loop
-3.times do
-	puts "ayo kita berhitung"
+2.times do
+  puts "ayo kita berhitung"
 end
 
 i = 0
 # while-loop
-while i <= 10 do
-	puts "hitungan ke #{i}"
-	i += 1
+while i <= 10
+  puts "hitungan ke #{i}"
+  i += 1
 end
 
 # for-loop
-for j in 11..20  do
-	puts "hitungan ke #{j}"
+for j in 11..20
+  puts "hitungan ke #{j}"
 end
 
 # each_method-loop
 (21..30).each do |k|
-	puts "hitungan ke #{k}"
+  puts "hitungan ke #{k}"
 end
 
 # until-loop
 l = 31
-until l > 40 do
-	puts "hitungan ke #{l}"
-	l += 1
+until l > 40
+  puts "hitungan ke #{l}"
+  l += 1
 end
