4c0f676e58786d33a949f80e06e76250a9ddc62b
xpynim <xpynim@gmail.com>
2024-07-17 17:31:04 +0700

    additem

--- ruby/rb_proj/main.rb
+++ ruby/rb_proj/main.rb
@@ -1,54 +1,18 @@
-require "colorize"
+# Loop
+puts "Perulangan Loop"
+(0..5).each do |i|
+  puts i
+end
 
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-puts "this is ruby learning"
-
-class Colorizer
-  def self.print_colored_text
-    puts "this is blue text".colorize(:blue)
-    puts "this is red text".colorize(:red)
+# Rekursi
+puts "\nPerulangan Rekursi"
+def loop_rekursi(angka)
+  if angka == 5
+    puts angka
+  else
+    puts angka
+    loop_rekursi(angka + 1)
   end
 end
 
-Colorizer.print_colored_text
-
-# tiga jenis iterasi umum
-
-# times-loop
-2.times do
-  puts "ayo kita berhitung"
-end
-
-i = 0
-# while-loop
-while i <= 10
-  puts "hitungan ke #{i}"
-  i += 1
-end
-
-# for-loop
-for j in 11..20
-  puts "hitungan ke #{j}"
-end
-
-# each_method-loop
-(21..30).each do |k|
-  puts "hitungan ke #{k}"
-end
-
-# until-loop
-l = 31
-until l > 40
-  puts "hitungan ke #{l}"
-  l += 1
-end
+loop_rekursi(0)
---
+++ ruby/rb_proj/past.rb
@@ -0,0 +1,54 @@
+require "colorize"
+
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+puts "this is ruby learning"
+
+class Colorizer
+  def self.print_colored_text
+    puts "this is blue text".colorize(:blue)
+    puts "this is red text".colorize(:red)
+  end
+end
+
+Colorizer.print_colored_text
+
+# tiga jenis iterasi umum
+
+# times-loop
+2.times do
+  puts "ayo kita berhitung"
+end
+
+i = 0
+# while-loop
+while i <= 10
+  puts "hitungan ke #{i}"
+  i += 1
+end
+
+# for-loop
+for j in 11..20
+  puts "hitungan ke #{j}"
+end
+
+# each_method-loop
+(21..30).each do |k|
+  puts "hitungan ke #{k}"
+end
+
+# until-loop
+l = 31
+until l > 40
+  puts "hitungan ke #{l}"
+  l += 1
+end
