555a60c707ed5ad7c4251e4000e55632806b7cd3
xpynim <xpynim@gmail.com>
2024-07-17 10:31:35 +0700

    update main.rb

--- ruby/rb_proj/main.rb
+++ ruby/rb_proj/main.rb
@@ -11,6 +11,7 @@ puts "this is ruby learning"
 puts "this is ruby learning"
 puts "this is ruby learning"
 puts "this is ruby learning"
+puts "this is ruby learning"
 
 class Colorizer
 	def self.print_colored_text
