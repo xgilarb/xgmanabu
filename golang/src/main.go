2e2cf66a5fc75a5773da5001b02beb8610f5c89b
xgilarb <gilarb@tuta.io>
2024-07-29 07:15:22 +0700

    update and build main.go

--- golang/src/main.go
+++ golang/src/main.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	} else {
+		return n * factorial(n-1)
+	}
+}
+
 func main() {
-	// komen sama seperti js/ts, kotlin, java
-	fmt.Println("hello go-world!")
-	fmt.Println("here we go again")
-	fmt.Println("and again")
-	fmt.Println("and again")
+	fmt.Println(factorial(5)) // Output: 120
 }
