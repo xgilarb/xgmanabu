1527d652be4d10af9921c6e453f9bace3659caf7
xgilarb <gilarb@tuta.io>
2024-07-31 15:09:34 +0700

    update main.go

--- golang/src/main.go
+++ golang/src/main.go
@@ -1,15 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-func factorial(n int) int {
-	if n == 0 {
-		return 1
-	} else {
-		return n * factorial(n-1)
-	}
+// Struct for book data
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+// Sample book list
+var books = []Book{
+	{ID: 1, Title: "Go Programming Language", Author: "Alan A. A. Donovan"},
+	{ID: 2, Title: "Learning Go", Author: "Jon Bodner"},
+	{ID: 3, Title: "The Way to Go", Author: "Ivo Balbaert"},
+}
+
+// Handler to get the book list in JSON format
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
-	fmt.Println(factorial(5)) // Output: 120
+	http.HandleFunc("/books", getBooks) // Assign handler to '/books' endpoint
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
